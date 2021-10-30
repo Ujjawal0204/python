442aa6243c57f23a67307dfb3dbad6d513fe5e34
Ujjawal Jaiswal <jaiswal1234uj@gmail.com>
2021-10-30 12:07:56 +0530

    new file

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,13 +3,14 @@ list_made=[]
 
 for loop in range(0,lenght):
     element=int(input("Enter Element: "))
-    list_made.append(element)
+    list_mrra
+    de.append(element)
 print("You made this list {0}".format(list_made))
-#bubble sorting
+#bubbl  e sorting
 for i in range(lenght):             
-    for j in range(0,lenght-i-1):                                   
-        if list_made[j]>list_made[j+1]:                                                 
-            list_made[j],list_made[j+1]=list_made[j+1],list_made[j]
+    for j in range(0, lenght-i-1):                                   
+        if list_made[j]made[j+1]:                                                 
+            list_made[j],list_me[j+1]=list_made[j+1],list_made[j]
 
 
-print("\nThe sorted list: {0} ".format(list_made))
+print("\nThe sorted list: {0} ".format(list_ma))
